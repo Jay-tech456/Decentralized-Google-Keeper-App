05f562b2e23c224583b2714895621e4b44fa2c12
Manjesh Prasad <manjeshprasad@Manjeshs-MacBook-Air.local>
2024-03-25 17:46:59 -0700

    Updated Decentralized Functionality

--- src/can_frontend/src/App.jsx
+++ src/can_frontend/src/App.jsx
@@ -1,30 +1,58 @@
-import { useState } from 'react';
-import { can_backend } from 'declarations/can_backend';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Note from "./components/Note";
+import CreateArea from "./components/CreateArea";
+import {can_backend} from "../../declarations/can_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [notes, setNotes] = useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    can_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+  function addNote(newNote) {
+    can_backend.createNote(newNote.title, newNote.content);
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
     });
-    return false;
+  }
+
+  useEffect(() => { 
+
+    console.log("Use Effect is triggered");
+    fetchData(); 
+  }, []); 
+
+
+  async function fetchData(){ 
+    const notesArray = await can_backend.readNotes(); 
+    setNotes(notesArray); 
+  }
+
+  function deleteNote(id) {
+    can_backend.removeNote(id); 
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
   }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
   );
 }
 
